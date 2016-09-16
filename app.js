ec6b273b65939ee6fcc400c6d00079792dbad368
John <losthuntet27@gmail.com>
2016-09-16 14:58:39 -0400

    Enabled node server logging

--- app.js
+++ app.js
@@ -5,16 +5,17 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
-// var morgan = require('morgan');
+var logger = require('morgan');
 var exec = require('child_process').exec;
 var bodyParser = require('body-parser');
 var app = express();
 
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));  //all html in public
 
 console.log("__dirname: "+__dirname);
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('photosbyjac.com node server listening on port 3000');
 });
